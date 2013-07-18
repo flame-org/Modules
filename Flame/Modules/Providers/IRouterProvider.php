08ae11e384b8aaf50121410c2497516378415fde
JSifalda <sifalda.jiri@gmail.com>
2013-07-18 11:43:03 +0200

    Add IRouterProvider

---
+++ Flame/Modules/Providers/IRouterProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class IRouterProvider
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 17.07.13
+ */
+namespace Flame\Modules\Providers;
+
+interface IRouterProvider
+{
+	/**
+	 * Returns array of ServiceDefinition,
+	 * that will be appended to setup of router service
+	 */
+	function getRoutesDefinition();
+}
