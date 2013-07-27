258b2ae09a718c1b7a10762349bd09e1d955333c
JSifalda <sifalda.jiri@gmail.com>
2013-07-27 14:47:30 +0200

    Add example of implementation

--- Flame/Modules/Providers/IRouterProvider.php
+++ Flame/Modules/Providers/IRouterProvider.php
@@ -9,9 +9,18 @@ namespace Flame\Modules\Providers;
 
 interface IRouterProvider
 {
+
 	/**
 	 * Returns array of ServiceDefinition,
 	 * that will be appended to setup of router service
+	 *
+	 * @example
+	 * return array(
+	 *      array('Nette\Application\Routers\Route' => array('/', array(
+	 *          'presenter' => 'Homepage',
+	 *          'action' => 'default'
+	 *      )))
+	 * );
 	 */
 	function getRoutesDefinition();
 }
