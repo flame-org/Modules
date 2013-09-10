ec20483ed5daa832a4bc713d78ba1b9aaef3c184
JSifalda <sifalda.jiri@gmail.com>
2013-09-10 20:05:10 +0200

    Add compatibility hack for ClassType class

--- Flame/Modules/compatibility.php
+++ Flame/Modules/compatibility.php
@@ -5,3 +5,7 @@ if (!class_exists('Nette\DI\CompilerExtension')) {
 	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
 	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
 }
+
+if(!class_exists('Nette\PhpGenerator\ClassType')) {
+	class_alias('Nette\Utils\PhpGenerator\ClassType', 'Nette\PhpGenerator\ClassType');
+}
