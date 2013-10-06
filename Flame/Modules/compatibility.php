f59c05b504d5698846fb58ca0ab04e6d804d362b
JSifalda <sifalda.jiri@gmail.com>
2013-10-06 11:59:35 +0200

    Check class exists

--- Flame/Modules/compatibility.php
+++ Flame/Modules/compatibility.php
@@ -1,11 +1,19 @@
 <?php
 
 if (!class_exists('Nette\DI\CompilerExtension')) {
-	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
-	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+	if(class_exists('Nette\Config\CompilerExtension')) {
+		class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	}
+
+	if(class_exists('Nette\Config\Compiler')) {
+		class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	}
+
+	if(class_exists('Nette\Config\Helpers')) {
+		class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+	}
 }
 
-if(!class_exists('Nette\PhpGenerator\ClassType')) {
+if(!class_exists('Nette\PhpGenerator\ClassType') && class_exists('Nette\Utils\PhpGenerator\ClassType')) {
 	class_alias('Nette\Utils\PhpGenerator\ClassType', 'Nette\PhpGenerator\ClassType');
 }
