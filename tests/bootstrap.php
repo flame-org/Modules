636140b9e00ebc0e5f70e212d63aecce9c66862c
Budry <zarubaondra@gmail.com>
2014-04-20 15:10:53 +0200

    Added tests for PresenterMappingProvider

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/../libs/autoload.php';
 require __DIR__ . '/data/ErrorPresenterExtension.php';
+require __DIR__ . '/data/PresenterMappingExtension.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
