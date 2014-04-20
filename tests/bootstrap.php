e1fa8b251ef2491c95e813c49236f7d2e2cbfb8e
Budry <zarubaondra@gmail.com>
2014-04-20 12:40:46 +0200

    Added container creator

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../libs/autoload.php';
+require __DIR__ . '/data/ErrorPresenterExtension.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
@@ -19,4 +20,12 @@ if (extension_loaded('xdebug')) {
  */
 function run(\Tester\TestCase $testCase) {
 	$testCase->run(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL);
+}
+
+function getContainer() {
+	$configurator = new \Nette\Configurator();
+	$configurator->setTempDirectory(__DIR__ . '/temp')
+		->addConfig(__DIR__ . '/data/config.neon');
+
+	return $configurator->createContainer();
 }
