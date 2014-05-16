351ac01833e0bcf7c466cec37512971fe127de72
JSifalda <sifalda.jiri@gmail.com>
2014-05-16 09:23:36 +0200

    Move into "unit" subfolder

---
+++ tests/unit/temp/gicdy/cache/Nette.Configurator/9273674b5d7fbb792f96940f3c7ecb59.php
@@ -0,0 +1,410 @@
+<?php
+// source: /Users/jsifalda/projects/Modules/tests/unit/data/config.neon 
+
+/**
+ * @property Nette\Application\Application $application
+ * @property Nette\Caching\Storages\FileStorage $cacheStorage
+ * @property Nette\DI\Container $container
+ * @property Nette\Http\Request $httpRequest
+ * @property Nette\Http\Response $httpResponse
+ * @property Nette\Bridges\Framework\NetteAccessor $nette
+ * @property Nette\Application\Routers\RouteList $router
+ * @property Nette\Http\Session $session
+ * @property Nette\Security\User $user
+ */
+class SystemContainer extends Nette\DI\Container
+{
+
+	protected $meta = array(
+		'types' => array(
+			'nette\\object' => array(
+				'nette',
+				'nette.cacheJournal',
+				'cacheStorage',
+				'nette.cache',
+				'nette.httpRequestFactory',
+				'httpRequest',
+				'httpResponse',
+				'nette.httpContext',
+				'session',
+				'nette.userStorage',
+				'user',
+				'application',
+				'nette.presenterFactory',
+				'router',
+				'nette.mailer',
+				'nette.templateFactory',
+				'container',
+			),
+			'nette\\bridges\\framework\\netteaccessor' => array('nette'),
+			'nette\\caching\\storages\\ijournal' => array('nette.cacheJournal'),
+			'nette\\caching\\storages\\filejournal' => array('nette.cacheJournal'),
+			'nette\\caching\\istorage' => array('cacheStorage'),
+			'nette\\caching\\storages\\filestorage' => array('cacheStorage'),
+			'arrayaccess' => array('nette.cache', 'router'),
+			'nette\\caching\\cache' => array('nette.cache'),
+			'nette\\http\\requestfactory' => array('nette.httpRequestFactory'),
+			'nette\\http\\irequest' => array('httpRequest'),
+			'nette\\http\\request' => array('httpRequest'),
+			'nette\\http\\iresponse' => array('httpResponse'),
+			'nette\\http\\response' => array('httpResponse'),
+			'nette\\http\\context' => array('nette.httpContext'),
+			'nette\\http\\session' => array('session'),
+			'nette\\security\\iuserstorage' => array('nette.userStorage'),
+			'nette\\http\\userstorage' => array('nette.userStorage'),
+			'nette\\security\\user' => array('user'),
+			'nette\\application\\application' => array('application'),
+			'nette\\application\\ipresenterfactory' => array('nette.presenterFactory'),
+			'nette\\application\\presenterfactory' => array('nette.presenterFactory'),
+			'nette\\utils\\arraylist' => array('router'),
+			'traversable' => array('router'),
+			'iteratoraggregate' => array('router'),
+			'countable' => array('router'),
+			'nette\\application\\irouter' => array('router'),
+			'nette\\application\\routers\\routelist' => array('router'),
+			'nette\\mail\\imailer' => array('nette.mailer'),
+			'nette\\mail\\sendmailmailer' => array('nette.mailer'),
+			'nette\\bridges\\applicationlatte\\ilattefactory' => array('nette.latteFactory'),
+			'nette\\application\\ui\\itemplatefactory' => array('nette.templateFactory'),
+			'nette\\bridges\\applicationlatte\\templatefactory' => array('nette.templateFactory'),
+			'flame\\modules\\template\\ihelperprovider' => array('0.helperProvider0'),
+			'testhelper2' => array('0.helperProvider0'),
+			'testhelper' => array('5.testHelper'),
+			'nette\\di\\container' => array('container'),
+		),
+	);
+
+
+	public function __construct()
+	{
+		parent::__construct(array(
+			'appDir' => '/Users/jsifalda/projects/Modules/tests/unit',
+			'wwwDir' => NULL,
+			'debugMode' => FALSE,
+			'productionMode' => TRUE,
+			'environment' => 'production',
+			'consoleMode' => FALSE,
+			'container' => array(
+				'class' => 'SystemContainer',
+				'parent' => 'Nette\\DI\\Container',
+				'accessors' => TRUE,
+			),
+			'tempDir' => '/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy',
+		));
+	}
+
+
+	/**
+	 * @return TestHelper2
+	 */
+	public function createService__0__helperProvider0()
+	{
+		$service = new TestHelper2;
+		return $service;
+	}
+
+
+	/**
+	 * @return TestHelper
+	 */
+	public function createService__5__testHelper()
+	{
+		$service = new TestHelper;
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Application\Application
+	 */
+	public function createServiceApplication()
+	{
+		$service = new Nette\Application\Application($this->getService('nette.presenterFactory'), $this->getService('router'), $this->getService('httpRequest'), $this->getService('httpResponse'));
+		$service->catchExceptions = TRUE;
+		$service->errorPresenter = 'Nette:Error';
+		Nette\Bridges\ApplicationTracy\RoutingPanel::initializePanel($service);
+		$service->errorPresenter = 'Flame:Module:Error';
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Caching\Storages\FileStorage
+	 */
+	public function createServiceCacheStorage()
+	{
+		$service = new Nette\Caching\Storages\FileStorage('/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy/cache', $this->getService('nette.cacheJournal'));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\DI\Container
+	 */
+	public function createServiceContainer()
+	{
+		return $this;
+	}
+
+
+	/**
+	 * @return Nette\Http\Request
+	 */
+	public function createServiceHttpRequest()
+	{
+		$service = $this->getService('nette.httpRequestFactory')->createHttpRequest();
+		if (!$service instanceof Nette\Http\Request) {
+			throw new Nette\UnexpectedValueException('Unable to create service \'httpRequest\', value returned by factory is not Nette\\Http\\Request type.');
+		}
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Http\Response
+	 */
+	public function createServiceHttpResponse()
+	{
+		$service = new Nette\Http\Response;
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Bridges\Framework\NetteAccessor
+	 */
+	public function createServiceNette()
+	{
+		$service = new Nette\Bridges\Framework\NetteAccessor($this);
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Caching\Cache
+	 */
+	public function createServiceNette__cache($namespace = NULL)
+	{
+		$service = new Nette\Caching\Cache($this->getService('cacheStorage'), $namespace);
+		trigger_error('Service cache is deprecated.', 16384);
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Caching\Storages\FileJournal
+	 */
+	public function createServiceNette__cacheJournal()
+	{
+		$service = new Nette\Caching\Storages\FileJournal('/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy');
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Http\Context
+	 */
+	public function createServiceNette__httpContext()
+	{
+		$service = new Nette\Http\Context($this->getService('httpRequest'), $this->getService('httpResponse'));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Http\RequestFactory
+	 */
+	public function createServiceNette__httpRequestFactory()
+	{
+		$service = new Nette\Http\RequestFactory;
+		$service->setProxy(array());
+		return $service;
+	}
+
+
+	/**
+	 * @return Latte\Engine
+	 */
+	public function createServiceNette__latte()
+	{
+		$service = new Latte\Engine;
+		$service->setTempDirectory('/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy/cache/latte');
+		$service->setAutoRefresh(FALSE);
+		$service->setContentType('html');
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Bridges\ApplicationLatte\ILatteFactory
+	 */
+	public function createServiceNette__latteFactory()
+	{
+		return new SystemContainer_Nette_Bridges_ApplicationLatte_ILatteFactoryImpl_nette_latteFactory($this);
+	}
+
+
+	/**
+	 * @return Nette\Mail\SendmailMailer
+	 */
+	public function createServiceNette__mailer()
+	{
+		$service = new Nette\Mail\SendmailMailer;
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Application\PresenterFactory
+	 */
+	public function createServiceNette__presenterFactory()
+	{
+		$service = new Nette\Application\PresenterFactory('/Users/jsifalda/projects/Modules/tests/unit', $this);
+		$service->setMapping(array(
+			'FlameModule' => 'Flame\\Test\\*Module\\Presenters\\*Presenter',
+		));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Templating\FileTemplate
+	 */
+	public function createServiceNette__template()
+	{
+		$service = new Nette\Templating\FileTemplate;
+		$service->registerFilter($this->getService('nette.latteFactory')->create());
+		$service->registerHelperLoader('Nette\\Templating\\Helpers::loader');
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Caching\Storages\PhpFileStorage
+	 */
+	public function createServiceNette__templateCacheStorage()
+	{
+		$service = new Nette\Caching\Storages\PhpFileStorage('/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy/cache', $this->getService('nette.cacheJournal'));
+		trigger_error('Service templateCacheStorage is deprecated.', 16384);
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Bridges\ApplicationLatte\TemplateFactory
+	 */
+	public function createServiceNette__templateFactory()
+	{
+		$service = new Nette\Bridges\ApplicationLatte\TemplateFactory($this->getService('nette.latteFactory'), $this->getService('httpRequest'), $this->getService('httpResponse'), $this->getService('user'), $this->getService('cacheStorage'));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Http\UserStorage
+	 */
+	public function createServiceNette__userStorage()
+	{
+		$service = new Nette\Http\UserStorage($this->getService('session'));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Application\Routers\RouteList
+	 */
+	public function createServiceRouter()
+	{
+		$service = new Nette\Application\Routers\RouteList;
+		Flame\Modules\Application\RouterFactory::prependTo($service, array(
+			Nette\PhpGenerator\Helpers::createObject('Flame\Modules\Application\Routers\NetteRouteMock', array(
+				'class' => 'Nette\\Application\\Routers\\Route',
+				'args' => array('test', 'FlameTestPresenter:', 0),
+				'router' => NULL,
+			)),
+			Nette\PhpGenerator\Helpers::createObject('Flame\Modules\Application\Routers\RouteMock', array(
+				'class' => 'Nette\\Application\\Routers\\Route',
+				'args' => array(
+					'test2',
+					array(
+						'module' => 'FlameModule',
+						'presenter' => 'FlamePresenter',
+					),
+				),
+				'router' => NULL,
+			)),
+		));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Http\Session
+	 */
+	public function createServiceSession()
+	{
+		$service = new Nette\Http\Session($this->getService('httpRequest'), $this->getService('httpResponse'));
+		return $service;
+	}
+
+
+	/**
+	 * @return Nette\Security\User
+	 */
+	public function createServiceUser()
+	{
+		$service = new Nette\Security\User($this->getService('nette.userStorage'));
+		return $service;
+	}
+
+
+	public function initialize()
+	{
+		date_default_timezone_set('Europe/Prague');
+		Nette\Bridges\Framework\TracyBridge::initialize();
+		Nette\Caching\Storages\FileStorage::$useDirectories = TRUE;
+		$this->getByType("Nette\Http\Session")->exists() && $this->getByType("Nette\Http\Session")->start();
+		header('X-Frame-Options: SAMEORIGIN');
+		header('X-Powered-By: Nette Framework');
+		header('Content-Type: text/html; charset=utf-8');
+		Nette\Utils\SafeStream::register();
+		Nette\Reflection\AnnotationsParser::setCacheStorage($this->getByType("Nette\Caching\IStorage"));
+		Nette\Reflection\AnnotationsParser::$autoRefresh = FALSE;
+	}
+
+}
+
+
+
+final class SystemContainer_Nette_Bridges_ApplicationLatte_ILatteFactoryImpl_nette_latteFactory implements Nette\Bridges\ApplicationLatte\ILatteFactory
+{
+
+	private $container;
+
+
+	public function __construct(Nette\DI\Container $container)
+	{
+		$this->container = $container;
+	}
+
+
+	public function create()
+	{
+		$service = new Latte\Engine;
+		$service->setTempDirectory('/Users/jsifalda/projects/Modules/tests/unit/temp/gicdy/cache/latte');
+		$service->setAutoRefresh(FALSE);
+		$service->setContentType('html');
+		TestMacro::install($service->getCompiler());
+		$service->addFilter('flamehelper', array(
+			Nette\PhpGenerator\Helpers::createObject('TestHelper', array()),
+			'process',
+		));
+		$service->addFilter('flamehelper2', array(
+			$this->container->getService('5.testHelper'),
+			'process',
+		));
+		Flame\Modules\Template\Helper::register($service, $this->container->getService('0.helperProvider0'));
+		return $service;
+	}
+
+}
