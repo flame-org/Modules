bf329fc5a666c19469b679c589b730f66cc1cf05
JSifalda <sifalda.jiri@gmail.com>
2014-05-17 15:19:10 +0200

    Add Tracy panels example

---
+++ tests/integration/app/TracyModule/Panels/MyBlueScreenPanel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ */
+namespace App\TracyModule\Panels;
+
+class MyBlueScreenPanel
+{
+
+	public static function test()
+	{
+		echo 'aa';
+	}
+
+} 
