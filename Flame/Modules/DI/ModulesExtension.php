08c5133d1462fe95f4dc2934b3f7d5a9af5dee23
Tomas Votruba <info@tomasvotruba.cz>
2014-08-21 15:12:12 +0200

    Validator added, coding standard, simplified

--- Flame/Modules/DI/ModulesExtension.php
+++ Flame/Modules/DI/ModulesExtension.php
@@ -7,7 +7,6 @@
  */
 namespace Flame\Modules\DI;
 
-use Flame\Modules\Application\Routers\IRouteMock;
 use Flame\Modules\Application\Routers\NetteRouteListMock;
 use Flame\Modules\Application\Routers\NetteRouteMock;
 use Flame\Modules\Application\Routers\RouteMock;
@@ -20,80 +19,63 @@ use Flame\Modules\Providers\ITemplateHelpersProvider;
 use Flame\Modules\Providers\ITracyBarPanelsProvider;
 use Flame\Modules\Providers\ITracyPanelsProvider;
 use Nette;
+use Nette\Utils\Validators;
+
 
 class ModulesExtension extends Nette\DI\CompilerExtension
 {
 
-	/**
-	 * @return void
-	 */
 	public function loadConfiguration()
 	{
-		$builder = $this->getContainerBuilder();
-
-		$presenterFactory = $builder->getDefinition('nette.presenterFactory');
-		try {
-			$latte = $builder->getDefinition('nette.latteFactory');
-		} catch (Nette\DI\MissingServiceException $e) {
-			$latte = $builder->getDefinition('nette.latte');
-		}
-		$application = $builder->getDefinition('application');
-		$router = $builder->getDefinition('router');
-
-		$extensions = $this->compiler->getExtensions();
-		foreach ($extensions as $extension) {
+		foreach ($this->compiler->getExtensions() as $extension) {
 			if ($extension instanceof IParametersProvider) {
-				$this->setupParameters($builder, $extension);
+				$this->setupParameters($extension);
 			}
 
 			if ($extension instanceof IPresenterMappingProvider) {
-				$this->setupPresenterMapping($presenterFactory, $extension);
+				$this->setupPresenterMapping($extension);
 			}
 
 			if ($extension instanceof IRouterProvider) {
-				$this->setupRouter($router, $extension);
+				$this->setupRouter($extension);
 			}
 
-			if($extension instanceof ILatteMacrosProvider) {
-				$this->setupMacros($latte, $extension);
+			if ($extension instanceof ILatteMacrosProvider) {
+				$this->setupMacros($extension);
 			}
 
-			if($extension instanceof ITemplateHelpersProvider) {
-				$this->setupHelpers($latte, $extension);
+			if ($extension instanceof ITemplateHelpersProvider) {
+				$this->setupHelpers($extension);
 			}
 
-			if($extension instanceof IErrorPresenterProvider){
-				$this->setupErrorPresenter($application, $extension);
+			if ($extension instanceof IErrorPresenterProvider){
+				$this->setupErrorPresenter($extension);
 			}
 		}
 	}
 
-	/**
-	 * @param Nette\PhpGenerator\ClassType $class
-	 * @return void
-	 */
 	public function afterCompile(Nette\PhpGenerator\ClassType $class)
 	{
-		$container = $this->getContainerBuilder();
+		$builder = $this->getContainerBuilder();
 
-		if ($container->parameters['debugMode'] && isset($class->methods['initialize'])) {
+		if ($builder->parameters['debugMode']) {
 			$initialize = $class->methods['initialize'];
 
 			foreach ($this->compiler->getExtensions() as $extension) {
 				if ($extension instanceof ITracyBarPanelsProvider) {
 					foreach ($extension->getTracyBarPanels() as $item) {
-						$initialize->addBody($container->formatPhp(
+						$initialize->addBody($builder->formatPhp(
 							'Nette\Diagnostics\Debugger::getBar()->addPanel(?);',
-							Nette\DI\Compiler::filterArguments(array(is_string($item) ? new Nette\DI\Statement($item) : $item))
+							Nette\DI\Compiler::filterArguments([is_string($item) ? new Nette\DI\Statement($item) : $item])
 						));
 					}
 				}
 
 				if ($extension instanceof ITracyPanelsProvider) {
 					foreach ($extension->getTracyPanels() as $item) {
-						$initialize->addBody($container->formatPhp(
+						$initialize->addBody($builder->formatPhp(
 							'Nette\Diagnostics\Debugger::getBlueScreen()->addPanel(?);',
-							Nette\DI\Compiler::filterArguments(array($item))
+							Nette\DI\Compiler::filterArguments([$item])
 						));
 					}
 				}
@@ -101,182 +83,146 @@ class ModulesExtension extends Nette\DI\CompilerExtension
 		}
 	}
 
-	/**
-	 * @param Nette\DI\ContainerBuilder $builder
-	 * @param IParametersProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupParameters(Nette\DI\ContainerBuilder &$builder, IParametersProvider &$extension)
+	private function setupParameters(IParametersProvider $extension)
 	{
 		$parameters = $extension->getParameters();
-		if (!is_array($parameters)) {
-			throw new Nette\InvalidStateException('Definition of parameters must be in array');
-		}
+		Validators::assert($parameters, 'array', 'parameters');
 
+		$builder = $this->getContainerBuilder();
 		if (count($parameters)) {
 			$builder->parameters = Nette\DI\Config\Helpers::merge($builder->expand($parameters), $builder->parameters);
 		}
 	}
 
-	/**
-	 * @param Nette\DI\ServiceDefinition $latte
-	 * @param ILatteMacrosProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupMacros(Nette\DI\ServiceDefinition &$latte, ILatteMacrosProvider &$extension)
+	private function setupMacros(ILatteMacrosProvider $extension)
 	{
 		$macros = $extension->getLatteMacros();
-		if(!is_array($macros)) {
-			throw new Nette\InvalidStateException('Definition of Latte macros must be in array.');
-		}
+		Validators::assert($macros, 'array', 'macros');
 
-		if(count($macros)) {
-			foreach ($macros as $macro) {
-				if (strpos($macro, '::') === FALSE && class_exists($macro)) {
-					$macro .= '::install';
-				} else {
-					if(!is_callable($macro)) {
-						throw new Nette\InvalidStateException('Macro must be callable.');
-					}
-				}
+		$latteFactory = $this->getLatteFactory();
+		foreach ($macros as $macro) {
+			if (strpos($macro, '::') === FALSE && class_exists($macro)) {
+				$macro .= '::install';
 
-				$latte->addSetup('?->onCompile[] = function($engine) { ' . $macro . '($engine->getCompiler()); }', array('@self'));
+			} else {
+				Validators::assert($macro, 'callable', 'macro');
 			}
+
+			$latteFactory->addSetup($macro . '(?->getCompiler());', ['@self']);
 		}
 	}
 
-	/**
-	 * @param Nette\DI\ServiceDefinition $latte
-	 * @param ITemplateHelpersProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupHelpers(Nette\DI\ServiceDefinition &$latte, ITemplateHelpersProvider &$extension)
+	private function setupHelpers(ITemplateHelpersProvider $extension)
 	{
 		$helpers = $extension->getHelpersConfiguration();
-		if (!is_array($helpers)) {
-			throw new Nette\InvalidStateException('Definition of Latte helpers must be in array.');
-		}
+		Validators::assert($helpers, 'array', 'helpers');
 
-		if(count($helpers)) {
-			$builder = $this->getContainerBuilder();
+		$builder = $this->getContainerBuilder();
+		$latteFactory = $this->getLatteFactory();
+		if (count($helpers)) {
 			foreach ($helpers as $name => $helper) {
-				if(is_string($helper) && !is_string($name)) {
-					$provider = $builder->addDefinition($this->prefix('helperProvider' . $name))
+				if (is_string($helper) && !is_string($name)) {
+					$provider = $builder->addDefinition($this->prefix('helperProvider.' . $name))
 						->setClass($helper);
 
-					$latte->addSetup('Flame\Modules\Template\Helper::register($service, ?)', array($provider));
-				}else{
-					if(!is_string($name)) {
-						throw new Nette\InvalidStateException('Template helper\'s name must be specified, "' . $name . '" given!');
-					}
+					$latteFactory->addSetup('Flame\Modules\Template\Helper::register($service, ?)', [$provider]);
 
-					if(!is_array($helper) && !is_string($helper)) {
-						throw new Nette\InvalidStateException('Template helper\'s definition must be array or string, "' . gettype($helper) . '" given');
-					}
+				} else {
+					Validators::assert($name, 'string', 'name');
+					Validators::assert($helper, 'string|array', 'helper');
 
-					$latte->addSetup('addFilter', array($name, $helper));
+					$latteFactory->addSetup('addFilter', [$name, $helper]);
 				}
 			}
 		}
 	}
 
-	/**
-	 * @param Nette\DI\ServiceDefinition $presenterFactory
-	 * @param IPresenterMappingProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupPresenterMapping(Nette\DI\ServiceDefinition &$presenterFactory, IPresenterMappingProvider &$extension)
+	private function setupPresenterMapping(IPresenterMappingProvider $extension)
 	{
 		$mapping = $extension->getPresenterMapping();
-		if(!is_array($mapping)) {
-			throw new Nette\InvalidStateException('Presenter mapping must be in array.');
-		}
+		Validators::assert($mapping, 'array', 'mapping');
 
 		if (count($mapping)) {
-			$presenterFactory->addSetup('setMapping', array($mapping));
+			$this->getContainerBuilder()->getDefinition('nette.presenterFactory')
+				->addSetup('setMapping', [$mapping]);
 		}
 	}
 
-	/**
-	 * @param Nette\DI\ServiceDefinition $router
-	 * @param IRouterProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupRouter(Nette\DI\ServiceDefinition &$router, IRouterProvider &$extension)
+	private function setupRouter(IRouterProvider $extension)
 	{
 		$routes = $extension->getRoutesDefinition();
-		if(!is_array($routes)) {
-			throw new Nette\InvalidStateException('Routes definition must be in array.');
-		}
+		Validators::assert($routes, 'array', 'routes');
+
+		$builder = $this->getContainerBuilder();
+		$router = $builder->getDefinition('router');
 
 		if (count($routes)) {
 			foreach ($routes as &$service) {
-
 				if ($service instanceof Nette\Application\Routers\Route
 					|| $service instanceof Nette\Application\Routers\RouteList) {
-
 					$service = $this->createRouteMock($service);
 
-				}elseif (is_array($service) && count($service) >= 1) {
-
+				} elseif (is_array($service) && count($service) >= 1) {
 					$class = key($service);
 					$service = new RouteMock($class, $service[$class]);
 
-				}elseif(is_string($service)) {
-
+				} elseif (is_string($service)) {
 					$service = new RouteMock($service);
 				}
 
-				if(!$service instanceof IRouteMock){
-					throw new Nette\InvalidStateException('Unsupported Route type "' . gettype($service) . '" given, try create Nette\Application\Routers\Route object.');
-				}
+				Validators::assert($service, 'Flame\Modules\Application\Routers\IRouteMock');
 
 				// In the future use this instead of RouterFactory
 				//$router->addSetup('offsetSet', array(NULL, $service));
 			}
 
-			$router->addSetup('Flame\Modules\Application\RouterFactory::prependTo($service, ?)', array($routes));
+			$router->addSetup('Flame\Modules\Application\RouterFactory::prependTo($service, ?)', [$routes]);
 		}
 	}
 
 	/**
 	 * @param mixed $route
 	 * @return array|NetteRouteListMock|NetteRouteMock
-	 * @throws \Nette\InvalidStateException
 	 */
 	private function createRouteMock($route)
 	{
+		Validators::assert($route, 'Nette\Application\Routers\Route|Nette\Application\Routers\RouteList');
 
 		if ($route instanceof Nette\Application\Routers\Route) {
-
 			return new NetteRouteMock($route->getMask(), $route->getDefaults(), $route->getFlags());
 
-		}elseif ($route instanceof Nette\Application\Routers\RouteList) {
+		} else {
 			$module = trim($route->getModule(), ':');
 			$mock = new NetteRouteListMock($module);
 
-			foreach($route as $item) {
+			foreach ($route as $item) {
 				$mock[] = $this->createRouteMock($item);
 			}
 
 			return $mock;
 		}
-
-		throw new Nette\InvalidStateException('Unsupported route type given.');
 	}
 
-	/**
-	 * @param Nette\DI\ServiceDefinition $application
-	 * @param IErrorPresenterProvider $extension
-	 * @throws \Nette\InvalidStateException
-	 */
-	private function setupErrorPresenter(Nette\DI\ServiceDefinition &$application, IErrorPresenterProvider &$extension)
+	private function setupErrorPresenter(IErrorPresenterProvider $extension)
 	{
 		$presenterName = $extension->getErrorPresenterName();
-		if(!is_string($presenterName)) {
-			throw new Nette\InvalidStateException('Presenter name must be string.');
-		}
+		Validators::assert($presenterName, 'string', 'presenterName');
 
-		$application->addSetup('$errorPresenter', array($presenterName));
+		$builder = $this->getContainerBuilder();
+		$builder->getDefinition('application')
+			->addSetup('$errorPresenter', [$presenterName]);
 	}
+
+
+	/**
+	 * @return Nette\DI\ServiceDefinition
+	 */
+	private function getLatteFactory()
+	{
+		$builder = $this->getContainerBuilder();
+		return $builder->hasDefinition('nette.latteFactory')
+			? $builder->getDefinition('nette.latteFactory')
+			: $builder->getDefinition('nette.latte');
+	}
+
 }
