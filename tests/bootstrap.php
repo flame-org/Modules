c9726d5e8112e7e41a55b75f4aadd8082b43261a
Tomas Votruba <info@tomasvotruba.cz>
2014-09-01 14:28:19 +0200

    tests: tmp dir added

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
-define('TEMP_DIR', __DIR__ . '/temp/' . Nette\Utils\Random::generate(5));
+define('TEMP_DIR', __DIR__ . '/tmp/' . Nette\Utils\Random::generate(5));
 Tester\Helpers::purge(TEMP_DIR);
 Tracy\Debugger::$logDirectory = TEMP_DIR;
 
